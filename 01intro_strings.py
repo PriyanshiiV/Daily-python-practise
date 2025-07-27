40e08f9e771b09db103bc48e64c1b029e247add7
Priyanshi Vishwakarma <ds202227@global.org.in>
2025-07-27 17:35:13 +0530

    Add files via upload

---
+++ 01intro_strings.py
@@ -0,0 +1,9 @@
+name = "priyanshi"
+#strings can made by " double" , 'single' , '''for multiple lines'''
+#strings are immutable
+
+nameshort =name[0:3]#start from index 0 all the way till 3 (excluding 3 )
+
+print(nameshort)
+character1 = name[1]
+print(character1) 
---
+++ 02negative_slicing.py
@@ -0,0 +1,10 @@
+name= "priyanshi"
+
+print(name[0:3])
+
+print(name[-4:-1 ])
+print(name[5:8])
+
+print(name[:4])#means 0 :4 likha hai 
+print(name[1:])#blank means length -1(it is same as[1:9])
+print(name[1:9])
