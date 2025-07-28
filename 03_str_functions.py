b7b6f9f7c2116708234e39487e23204feeebd9cb
Priyanshi Vishwakarma <ds202227@global.org.in>
2025-07-28 22:58:14 +0530

    Add files via upload

---
+++ 03_str_functions.py
@@ -0,0 +1,8 @@
+name = ("priyanshi")
+print(len(name))#len tells you lenth of character 
+
+print(name.endswith("shi"))
+print(name.startswith("pri"))
+
+print(name.capitalize())#only capatilize first letter
+print(name.title())
---
+++ 04_escape_seq.py
@@ -0,0 +1,3 @@
+a = "priyanshi\n is a \tgood girl\nbut not a\n bad girl"
+print(a)#\n is used for new line
+#\t give a tab spaces
