f7a3b3bbbe9106759aa86bd9e2a4634f7c27501e
Priyanshi Vishwakarma <ds202227@global.org.in>
2025-08-09 20:22:22 +0530

    Add files via upload

---
+++ 01_list.py
@@ -0,0 +1,5 @@
+friends = ["apple","orange",5 , "false ", "Akash"]
+print(friends [0])
+friends[0]="mangoes"#unlike strings lists are mutables
+print(friends[0])
+
---
+++ 02_list_methods.py
@@ -0,0 +1,28 @@
+#append method:
+friends = ["apple","orange",5 , "false ", "Akash"]
+print(friends)
+
+friends.append("priyanshi")
+
+print(friends)
+
+#sort method :
+l1 = [1.34,23,0,87,65,21,12,50.99]
+l1.sort()
+print(l1)
+
+#reverse method :
+l2 = ["aa", "bb", "cc", "dd", "ee", "ff","gg"]
+l2.reverse()
+print(l2)
+
+#insert method :
+l3=[1,23,500,3,43]
+l3.insert(3,99)
+print(l3)
+
+
+#pop method :
+l4=[10,20,30,44,40,50,60]
+l4.pop(3)
+print(l4)
